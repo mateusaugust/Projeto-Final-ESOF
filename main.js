a41e055eda00b84ae289894d837ba77c7f52d3d5
mateusaugust <mateusasilva033@gmail.com>
2023-05-29 17:34:02 -0300

    partial front-end

---
+++ main.js
@@ -0,0 +1,17 @@
+function Empresa(id,nome,cnpj, cpfFuncionario){
+  this.id=id;
+  this.nome=nome;
+  this.cnpj=cnpj
+  this.cpfFuncionario = cpfFuncionario;
+}
+
+var dados =[
+  new Empresa(1,"Empresa1", "CNPJ1","123456789-13"),
+  new Empresa(2,"Empresa2", "CNPJ2","123456789-13"),
+  new Empresa(3,"Empresa3", "CNPJ3","123456789-13"),
+  new Empresa(4,"Empresa4", "CNPJ5","123456789-13"),
+  new Empresa(5,"Empresa6", "CNPJ6","123456789-13"),
+  new Empresa(6,"Empresa7", "CNPJ7","123456789-13"),
+  new Empresa(7,"Empresa8", "CNPJ8","123456789-13")
+]
+
