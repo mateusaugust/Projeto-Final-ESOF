b04733249204cb88c6621f64ff216ee49d140a97
mateusaugust <mateusasilva033@gmail.com>
2023-05-29 22:17:59 -0300

    Alert

---
+++ Utils/telaBloaqueada.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+  checkProportion();
+  
+  $(window).resize(function() {
+    checkProportion();
+  });
+  
+  function checkProportion() {
+    var screenWidth = $(window).width();
+    var screenHeight = $(window).height();
+    
+    if (screenHeight <= 416 || screenWidth <=948) {
+      showOverlay();
+    } else{
+      hideOverlay();
+    }
+  }
+  
+  function showOverlay() {
+    $('#overlay').show();
+  }
+  
+  function hideOverlay() {
+    $('#overlay').hide();
+  }
+});
