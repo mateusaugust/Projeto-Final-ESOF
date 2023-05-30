b6ddbed84a2de6d849ff4eaf74be12f821fdc071
mateusaugust <mateusasilva033@gmail.com>
2023-05-30 09:47:09 -0300

    tela de login e cadastro ok

--- Utils/telaBloaqueada.js
+++ Utils/telaBloaqueada.js
@@ -1,15 +1,17 @@
+
 $(document).ready(function() {
   checkProportion();
+ 
   
   $(window).resize(function() {
     checkProportion();
   });
-  
+
   function checkProportion() {
     var screenWidth = $(window).width();
     var screenHeight = $(window).height();
-    
-    if (screenHeight <= 416 || screenWidth <=948) {
+
+    if (screenHeight <= 554 || screenWidth <=948) {
       showOverlay();
     } else{
       hideOverlay();
--- main.js
+++ main.js
@@ -5,6 +5,11 @@ function Empresa(id,nome,cnpj, cpfFuncionario){
   this.cpfFuncionario = cpfFuncionario;
 }
 
+function Cadastro(empresa,cnpj){
+    this.empresa=empresa;
+    this.cnpj=cnpj;
+}
+
 var dados =[
   new Empresa(1,"Empresa1", "CNPJ1","123456789-13"),
   new Empresa(2,"Empresa2", "CNPJ2","123456789-13"),
@@ -14,4 +19,21 @@ var dados =[
   new Empresa(6,"Empresa7", "CNPJ7","123456789-13"),
   new Empresa(7,"Empresa8", "CNPJ8","123456789-13")
 ]
+
+var dadosEmpresa =[
+  new Cadastro("Teste","123")
+]
+
+
+$(document).ready(function() {
+  $("#voltar").click(function() {
+    window.location.href = "../index.html";
+  });
+});
+
+$(document).ready(function(){
+  $('#login').click(function(){
+
+  });
+});
 
