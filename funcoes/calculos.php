a7e0ef5029f688bd8725618f80c10a361d145763
fabiano.pessoal@yahoo.com.br <fabiano.pessoal@yahoo.com.br>
2023-06-18 14:58:30 -0300

    parcial

--- funcoes/calculos.php
+++ funcoes/calculos.php
@@ -1,14 +1,28 @@
 <?php
 
+include_once("funcoesCalculos.php");
+
+
 $con = mysqli_connect("localhost", "root", "", "trabalhoesof");
 
-$sql = $dadosParaCalculo = "select * from caddemandas";
+// Verificar se a conexão foi estabelecida corretamente
+if (!$con) {
+    die("Erro na conexão: " . mysqli_connect_error());
+}
+
+$sql = "SELECT * FROM caddemandas WHERE ID = 23";
 
 $resultado = mysqli_query($con, $sql);
 
-echo $resultado;
+while ($row = mysqli_fetch_assoc($resultado)) {
+    // Exiba os dados de cada coluna
+    echo "ID: " . $row['ID'] . "<br>";
+    
+    // ... continue com as demais colunas
 
+    echo "<br>";
+}
 
-
-
+// Fechar a conexão com o banco de dados
+mysqli_close($con);
 ?>
