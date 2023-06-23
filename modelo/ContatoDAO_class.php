d558b7c26c42c31145ad7a0811a1eb5970a454e9
fabiano.pessoal@yahoo.com.br <fabiano.pessoal@yahoo.com.br>
2023-06-22 21:17:19 -0300

    Parcial

--- modelo/ContatoDAO_class.php
+++ modelo/ContatoDAO_class.php
@@ -22,15 +22,28 @@
 				"INSERT INTO caddemandas(, cpf,telefone, email,data_nascimento, sexo)
 				VALUES (:,:cpf , :telefone, :email, :data_nascimento, :sexo)");
 
+"INSERT INTO caddemandas(CNPJ, NOME, CPF, DATA_ADMISSAO, JORNADA_MENSAL, SALARIO, QUANT_DEPENDENTE, FERIAS_VENCIDAS, AD_PERICULOSIDADE, ADIC_INSALUBRIDADE, TIPO_TRCT, TIPO_AVISO, DATA_DEMISSAO, QUANT_HORA_EXTRA, SALDO_FGTS)
+VALUES ('[value-1]','[value-2]','[value-3]','[value-4]','[value-5]','[value-6]','[value-7]','[value-8]','[value-9]','[value-10]','[value-11]','[value-12]','[value-13]','[value-14]','[value-15]','[value-16]')";
+
 				
 				
 				//ligamos as âncoras aos valores de Contato
-				$stmt->bindValue(":nome", $cont->getNome());
-				$stmt->bindValue(":cpf", $cont->getCpf());
-				$stmt->bindValue(":telefone", $cont->getTelefone());
-				$stmt->bindValue(":email", $cont->getEmail());
-				$stmt->bindValue(":data_nascimento", $cont->getDataNascimento());
-				$stmt->bindValue(":sexo", $cont->getSexo());
+				$stmt->bindValue(":ID", $cont->getId());
+				$stmt->bindValue(":CNPJ", $cont->getCnpj());
+				$stmt->bindValue(":NOME", $cont->getNome());
+				$stmt->bindValue(":CPF", $cont->getCpf());
+				$stmt->bindValue(":DATA_ADMISSAO", $cont->getDataAdmissao());
+				$stmt->bindValue(":JORNADA_MENSAL", $cont->getJornadaMensal());
+				$stmt->bindValue(":SALARIO", $cont->getSalario());
+				$stmt->bindValue(":QUANT_DEPENDENTE", $cont->getQuantDependente());
+				$stmt->bindValue(":FERIAS_VENCIDAS", $cont->getFeriasVencidas());
+				$stmt->bindValue(":AD_PERICULOSIDADE", $cont->getAdPericulosidade());
+				$stmt->bindValue(":ADIC_INSALUBRIDADE", $cont->getAdicInsalubridade());
+				$stmt->bindValue(":TIPO_TRCT", $cont->getTipoTrct());
+				$stmt->bindValue(":TIPO_AVISO", $cont->getTipoAviso());
+				$stmt->bindValue(":DATA_DEMISSAO", $cont->getDataDemissao());
+				$stmt->bindValue(":QUANT_HORA_EXTRA", $cont->getQuantHoraExtra());
+				$stmt->bindValue(":SALDO_FGTS", $cont->getSaldoFGTS());
 
 				
 				
@@ -54,13 +67,22 @@
 				id=:id");
 				
 				//ligamos as âncoras aos valores de Contato
-				$stmt->bindValue(":nome", $cont->getNome());
-				$stmt->bindValue(":cpf", $cont->getCpf());
-				$stmt->bindValue(":telefone", $cont->getTelefone());
-				$stmt->bindValue(":email", $cont->getEmail());
-				$stmt->bindValue(":data_nascimento", $cont->getDataNascimento());
-				$stmt->bindValue(":sexo", $cont->getSexo());
-				$stmt->bindValue(":id", $cont->getId());
+				$stmt->bindValue(":ID", $cont->getId());
+				$stmt->bindValue(":CNPJ", $cont->getCnpj());
+				$stmt->bindValue(":NOME", $cont->getNome());
+				$stmt->bindValue(":CPF", $cont->getCpf());
+				$stmt->bindValue(":DATA_ADMISSAO", $cont->getDataAdmissao());
+				$stmt->bindValue(":JORNADA_MENSAL", $cont->getJornadaMensal());
+				$stmt->bindValue(":SALARIO", $cont->getSalario());
+				$stmt->bindValue(":QUANT_DEPENDENTE", $cont->getQuantDependente());
+				$stmt->bindValue(":FERIAS_VENCIDAS", $cont->getFeriasVencidas());
+				$stmt->bindValue(":AD_PERICULOSIDADE", $cont->getAdPericulosidade());
+				$stmt->bindValue(":ADIC_INSALUBRIDADE", $cont->getAdicInsalubridade());
+				$stmt->bindValue(":TIPO_TRCT", $cont->getTipoTrct());
+				$stmt->bindValue(":TIPO_AVISO", $cont->getTipoAviso());
+				$stmt->bindValue(":DATA_DEMISSAO", $cont->getDataDemissao());
+				$stmt->bindValue(":QUANT_HORA_EXTRA", $cont->getQuantHoraExtra());
+				$stmt->bindValue(":SALDO_FGTS", $cont->getSaldoFGTS());
 				
 				$this->con->beginTransaction();
 				//Inicia a transação
@@ -119,13 +141,22 @@
 				
 				foreach($dados as $linha){
 					$c = new Funcionario();
-					$c->setId($linha["id"]);
-					$c->setNome($linha["nome"]);
-					$c->setCpf($linha["cpf"]);
-					$c->setTelefone($linha["telefone"]);
-					$c->setEmail($linha["email"]);
-					$c->setDataNascimento($linha["data_nascimento"]);
-					$c->setSexo($linha["sexo"]);
+					$c->setId($linha["ID"]);
+					$c->setCnpj($linha["CNPJ"]);
+					$c->setNome($linha["NOME"]);
+					$c->setCpf($linha["CPF"]);
+					$c->setDataAdmissao($linha["DATA_ADMISSAO"]);
+					$c->setJornadaMensal($linha["JORNADA_MENSAL"]);
+					$c->setSalario($linha["SALARIO"]);
+					$c->setQuantDependente($linha["QUANT_DEPENDENTE"]);
+					$c->setFeriasVencidas($linha["FERIAS_VENCIDAS"]);
+					$c->setAdPericulosidade($linha["AD_PERICULOSIDADE"]);
+					$c->setAdicInsalubridade($linha["ADIC_INSALUBRIDADE"]);
+					$c->setTipoTrct($linha["TIPO_TRCT"]);
+					$c->setTipoAviso($linha["TIPO_AVISO"]);
+					$c->setDataDemissao($linha["DATA_DEMISSAO"]);
+					$c->setQuantHoraExtra($linha["QUANT_HORA_EXTRA"]);
+					$c->setSaldoFGTS($linha["SALDO_FGTS"]);
 					
 					$lista[] = $c;
 					//preenchendo um array com objetos Contato
@@ -150,14 +181,24 @@
 				$dado = $lista->fetchAll(PDO::FETCH_ASSOC);
 				
 				$c = new Funcionario();
-				$c->setId($dado[0]["id"]);
-				$c->setNome($dado[0]["nome"]);
-				$c->setCpf($dado[0]["cpf"]);
-				$c->setTelefone($dado[0]["telefone"]);
-				$c->setEmail($dado[0]["email"]);
-				$c->setDataNascimento($dado[0]["data_nascimento"]);
-				$c->setSexo($dado[0]["sexo"]);
 				
+				$c->setId($dado["ID"]);
+				$c->setCnpj($dado["CNPJ"]);
+				$c->setNome($dado["NOME"]);
+				$c->setCpf($dado["CPF"]);
+				$c->setDataAdmissao($dado["DATA_ADMISSAO"]);
+				$c->setJornadaMensal($dado["JORNADA_MENSAL"]);
+				$c->setSalario($dado["SALARIO"]);
+				$c->setQuantDependente($dado["QUANT_DEPENDENTE"]);
+				$c->setFeriasVencidas($dado["FERIAS_VENCIDAS"]);
+				$c->setAdPericulosidade($dado["AD_PERICULOSIDADE"]);
+				$c->setAdicInsalubridade($dado["ADIC_INSALUBRIDADE"]);
+				$c->setTipoTrct($dado["TIPO_TRCT"]);
+				$c->setTipoAviso($dado["TIPO_AVISO"]);
+				$c->setDataDemissao($dado["DATA_DEMISSAO"]);
+				$c->setQuantHoraExtra($dado["QUANT_HORA_EXTRA"]);
+				$c->setSaldoFGTS($dado["SALDO_FGTS"]);
+
 				return $c;	
 			}
 			catch(PDOException $ex){
--- visao/listaContato.php
+++ visao/listaContato.php
@@ -15,12 +15,23 @@
 		<TABLE border="1px" id="tabelaInicio">
 			<TR> 
 				<TH> ID </TH>
+				<TH> CNPJ </TH>
 				<TH> Nome </TH>
 				<TH> CPF </TH>
-				<TH> Telefone </TH>	
-				<TH> Email </TH>
-				<TH> Data Nascimento </TH>
-				<TH> Sexo </TH>		
+				<TH> Data de admissão </TH>
+				<TH> Jornada Mensal </TH>
+				<TH> Salario </TH>
+				<TH> nº depentende </TH>
+				<TH> nº Férias vencidas </TH>
+				<TH> Adic. Periculosidade </TH>
+				<TH> Adic. Insalubridade </TH>
+				<TH> Adic. Insalubridade </TH>
+				<TH> Tipo de TRCT </TH>
+				<TH> Tipo de Aviso </TH>
+				<TH> Data de demissão </TH>
+				<TH> Nº de horas extras </TH>
+				<TH> Saldo de FGTS </TH>
+				
 				
 				<TH> <img src="visao/img/update.png" width='30px' /> </TH>
 				<TH> <img src="visao/img/delete.png" width='30px' /> </TH>
@@ -32,20 +43,25 @@
 				foreach($lista as $c){	
 					echo "<TR>"; 
 					
+					
 					echo "<TD>" . $c->getId() . "</TD>";
-					
-					echo "<TD><A href=contato.php?fun=exibir&id=". $c->getId() . 
-					      ">" . $c->getNome() . "</A></TD>";
-
+					echo "<TD><A href=contato.php?fun=exibir&id=". $c->getId() . ">" . $c->getNome() . "</A></TD>";
+					echo "<TD>" . $c->getCnpj() . "</TD>";
 					echo "<TD>" . $c->getCpf() . "</TD>";
+					echo "<TD>" . $c->getDataAdmissao() . "</TD>";
+					echo "<TD>" . $c->getJornadaMensal() . "</TD>";	
+					echo "<TD>" . $c->getSalario() . "</TD>";	
+					echo "<TD>" . $c->getQuantDependente() . "</TD>";	
+					echo "<TD>" . $c->getFeriasVencidas() . "</TD>";	
+					echo "<TD>" . $c->getFeriasVencidas() . "</TD>";	
+					echo "<TD>" . $c->getAdPericulosidade() . "</TD>";	
+					echo "<TD>" . $c->getAdicInsalubridade() . "</TD>";	
+					echo "<TD>" . $c->getTipoTrct() . "</TD>";	
+					echo "<TD>" . $c->getTipoAviso() . "</TD>";	
+					echo "<TD>" . $c->getDataDemissao() . "</TD>";	
+					echo "<TD>" . $c->getQuantHoraExtra() . "</TD>";	
+					echo "<TD>" . $c->getSaldoFGTS() . "</TD>";	
 					
-					echo "<TD>" . $c->getTelefone() . "</TD>";
-					
-					echo "<TD>" . $c->getEmail() . "</TD>";
-					 
-					echo "<TD>" . $c->getDataNascimento() . "</TD>";	
-
-					echo "<TD>" . $c->getSexo() . "</TD>";	
 					
 					echo "<TD><A href=contato.php?fun=alterar&id=" . 
 					      $c->getId() . "><img src='visao/img/update.png' width='30px'/> 
