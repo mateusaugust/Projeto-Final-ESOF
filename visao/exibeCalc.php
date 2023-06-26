c5a0411d1b559de980c3af3d0aaa085275b05626
mateusaugust <mateusasilva033@gmail.com>
2023-06-25 22:43:41 -0300

    fix

--- visao/exibeCalc.php
+++ visao/exibeCalc.php
@@ -10,28 +10,28 @@
 
 	<BODY>
 		
-		<H3>  Dados Calculo (<?php echo $cont->getIdDemanda(); ?>) ?> </H3>
+		<H3>  Dados Calculo (<?php echo $cont->getIdDemanda(); ?>)</H3>
 		<UL>
 			<LI>ID DEMANDA: <?php echo $cont->getIdDemanda(); ?></LI>
-			<LI>SALARIO: <?php echo $cont1->getSalario(); ?> </LI>
-			<LI>AVOS DECIMO TERCEIRO: <?php echo $cont1->getAvosDecimoTerceiro(); ?> </LI>
-			<LI>DECIMO TERCEIRO: <?php echo $cont1->getDecimoTerceiro(); ?></LI>
-			<LI>AVOS FERIAS: <?php echo $cont1->getAvosFerias(); ?></LI>
-			<LI>FERIAS PROPORCIONAL: <?php echo $cont1->getFeriasProporcional(); ?></LI>
-			<LI>FERIAS VENCIDAS: <?php echo $cont1->getFeriasVencidas(); ?></LI>
-			<LI>TERÇO FERIAS: <?php echo $cont1->getTercoFerias(); ?></LI>
-			<LI>DIAS DE AVISO PREVIO: <?php echo $cont1->getDiasAvisoPrevio(); ?></LI>
-			<LI>AVISO PREVIO: <?php echo $cont1->getAvisoPrevio(); ?></LI>
-			<LI>QUANTIDADES DE HORAS EXTRAS: <?php echo $cont1->getQuantHoraExtra(); ?></LI>
-			<LI>VALOR DE HORAS EXTRAS: <?php echo $cont1->getValorHorasExtras(); ?></LI>
-			<LI>DSR HORAS EXTRAS: <?php echo $cont1->getDsrHorasExtras(); ?></LI>
-			<LI>PERCENTUAL DE INSALUBRIDADE: <?php echo $cont1->getPercInsalubridade(); ?></LI>
-			<LI>VALOR INSALUBRIDADE: <?php echo $cont1->getValorInsalubridade(); ?></LI>
-      <LI>VALOR PERICULOSIDADE: <?php echo $cont1->valor_periculosidade(); ?></LI>
-      <LI>SALARIO FAMILIA: <?php echo $cont1->getSalarioFamilia(); ?></LI>
-      <LI>INSS SALARIO: <?php echo $cont1->getInssSalario(); ?></LI>
-      <LI>INSS: <?php echo $cont1->getInss13(); ?></LI>
-      <LI>IR: <?php echo $cont1->getIr1(); ?></LI>
+			<LI>SALARIO: <?php echo $cont->getSalario(); ?> </LI>
+			<LI>AVOS DECIMO TERCEIRO: <?php echo $cont->getAvosDecimoTerceiro(); ?> </LI>
+			<LI>DECIMO TERCEIRO: <?php echo $cont->getDecimoTerceiro(); ?></LI>
+			<LI>AVOS FERIAS: <?php echo $cont->getAvosFerias(); ?></LI>
+			<LI>FERIAS PROPORCIONAL: <?php echo $cont->getFeriasProporcional(); ?></LI>
+			<LI>FERIAS VENCIDAS: <?php echo $cont->getFeriasVencidas(); ?></LI>
+			<LI>TERÇO FERIAS: <?php echo $cont->getTercoFerias(); ?></LI>
+			<LI>DIAS DE AVISO PREVIO: <?php echo $cont->getDiasAvisoPrevio(); ?></LI>
+			<LI>AVISO PREVIO: <?php echo $cont->getAvisoPrevio(); ?></LI>
+			<LI>QUANTIDADES DE HORAS EXTRAS: <?php echo $cont->getQuantidadesHorasExtras(); ?></LI>
+			<LI>VALOR DE HORAS EXTRAS: <?php echo $cont->getValorHorasExtras(); ?></LI>
+			<LI>DSR HORAS EXTRAS: <?php echo $cont->getDsrHorasExtras(); ?></LI>
+			<LI>PERCENTUAL DE INSALUBRIDADE: <?php echo $cont->getPercInsalubridade(); ?></LI>
+			<LI>VALOR INSALUBRIDADE: <?php echo $cont->getValorInsalubridade(); ?></LI>
+      <LI>VALOR PERICULOSIDADE: <?php echo $cont->getValorPericulosidade(); ?></LI>
+      <LI>SALARIO FAMILIA: <?php echo $cont->getSalarioFamilia(); ?></LI>
+      <LI>INSS SALARIO: <?php echo $cont->getInssSalario(); ?></LI>
+      <LI>INSS: <?php echo $cont->getInss13(); ?></LI>
+      <LI>IR: <?php echo $cont->getIr(); ?></LI>
 
 		</UL>
 		
